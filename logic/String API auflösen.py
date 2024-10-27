96e9ce7fb8b69a688583f664d952e83c76596af6
Turtle0815 <39406946+Turtle0815@users.noreply.github.com>
2024-10-27 11:17:38 +0100

    Update String API auflösen.py

--- logic/String API auflösen.py
+++ logic/String API auflösen.py
@@ -14,19 +14,21 @@ class API_auflösen():
     letzter_Datensatz = (API[x-1].split("},"))[0]
     letzter_Datensatz = letzter_Datensatz.split("'date': '")[1]    
     Daten.append(letzter_Datensatz)
-    print (Daten[1])
     x = len(Daten)
     i = 0
-    Datum = []
-    Rest = []
-    while i < x:
-        Datum_1 = Daten[i]
-        Datum_2 = Datum_1.split("T")
-        #Rest_1 = (Daten[i].split("; "))[1]
-        Datum.append(Datum_2)
-        #Rest.append(Rest)
+    Daten_1 = []
+    while i < x:                                                # trennt den String auf zur Weiterverarbeitung
+        String_Daten = Daten[i]
+        Liste_Daten = String_Daten.split("T")
+        Daten_1.append(Liste_Daten)                             # "T" verschwindet aus String, Datum und Uhrzeit sind getrennte Werte
+        Wert_Datum = Daten_1[i]
+        Tag = (Wert_Datum[0].split("-"))[2] 
+        Monat = (Wert_Datum[0].split("-"))[1]
+        Jahr = (Wert_Datum[0].split("-"))[0]                    # String in Tag, Monat, Jahr aufgetrennt
+        Wert_Rest = Daten_1[i]
+        Uhrzeit = (Wert_Rest[1].split("+"))[0]                  # Uhrzeit ohne + 2:00 Angabe
+        Wert_Preis= (Wert_Rest[1].split("', 'value': "))[1]     # Preis in Cent
+        # Hier erfolgt die Übergabe der ermittelten Werte in die Tabelle
         i = i+1
-    print (Datum[1])
-    #print (Rest[1])
     
                           
