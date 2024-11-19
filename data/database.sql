e49847bef79b1533a890dee1391def7c3b1cc9c2
richter-symanek <105888087+richter-symanek@users.noreply.github.com>
2024-11-19 03:11:36 +0100

    Update database.sql
    Delete two tables, update table names, add constraints for remaining tables and cascade on actio9n

--- data/database.sql
+++ data/database.sql
@@ -3,21 +3,10 @@ BEGIN;
 
 CREATE TABLE IF NOT EXISTS public.geraete
 (
-    geraeteid integer NOT NULL,
+    geraeteid integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
     name character varying(255) COLLATE pg_catalog."default" NOT NULL,
     soll_laufen boolean NOT NULL DEFAULT false,
     CONSTRAINT geraete_pkey PRIMARY KEY (geraeteid)
-);
-
-CREATE TABLE IF NOT EXISTS public.haushalt
-(
-    name character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    strasse character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    hausnummer integer NOT NULL,
-    plz integer NOT NULL,
-    ort character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    anschlussid integer NOT NULL,
-    CONSTRAINT anschlussid PRIMARY KEY (anschlussid)
 );
 
 CREATE TABLE IF NOT EXISTS public.laufzeitregeln
@@ -32,7 +21,7 @@ CREATE TABLE IF NOT EXISTS public.preise
 (
     stromanbieterid integer NOT NULL,
     preishoehe double precision NOT NULL,
-    gueltig_ab date NOT NULL,
+    gueltig_ab timestamp without time zone NOT NULL,
     CONSTRAINT preise_pkey PRIMARY KEY (stromanbieterid, gueltig_ab)
 );
 
@@ -41,14 +30,6 @@ CREATE TABLE IF NOT EXISTS public.stromanbieter
     stromanbieterid integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
     name character varying(255) COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT stromanbieter_pkey PRIMARY KEY (stromanbieterid)
-);
-
-CREATE TABLE IF NOT EXISTS public.vertraege
-(
-    anschlussid integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-    vertragsnummer character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    stromanbieterid integer NOT NULL,
-    CONSTRAINT vertraege_pkey PRIMARY KEY (anschlussid, vertragsnummer, stromanbieterid)
 );
 
 ALTER TABLE IF EXISTS public.laufzeitregeln
@@ -62,30 +43,12 @@ CREATE INDEX IF NOT EXISTS fki_geraeteid
 
 
 ALTER TABLE IF EXISTS public.preise
-    ADD CONSTRAINT stromanbieterid FOREIGN KEY (stromanbieterid)
+    ADD CONSTRAINT preise_stromanbieterid_fkey FOREIGN KEY (stromanbieterid)
     REFERENCES public.stromanbieter (stromanbieterid) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
     NOT VALID;
-
-
-ALTER TABLE IF EXISTS public.vertraege
-    ADD CONSTRAINT anschlussid FOREIGN KEY (anschlussid)
-    REFERENCES public.haushalt (anschlussid) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID;
-CREATE INDEX IF NOT EXISTS fki_anschlussid
-    ON public.vertraege(anschlussid);
-
-
-ALTER TABLE IF EXISTS public.vertraege
-    ADD CONSTRAINT stromanbieterid FOREIGN KEY (stromanbieterid)
-    REFERENCES public.stromanbieter (stromanbieterid) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID;
-CREATE INDEX IF NOT EXISTS fki_stromanbieterid
-    ON public.vertraege(stromanbieterid);
+CREATE INDEX IF NOT EXISTS fki_s
+    ON public.preise(stromanbieterid);
 
 END;
